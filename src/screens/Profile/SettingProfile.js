9e257babbd73c980a9e64214c01231cc408957b4
hieppn <hiep.bui21@student.passerellesnumeriques.org>
2021-01-28 17:34:31 +0700

    Hiep_fixed

--- src/screens/Profile/SettingProfile.js
+++ src/screens/Profile/SettingProfile.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
 import SettingProfileItem from '../../components/SettingProfileItem';
 const SettingProfile = (props) => {
